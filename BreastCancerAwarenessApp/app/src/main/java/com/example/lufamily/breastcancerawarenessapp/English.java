fbdf8302369da66c9d172c5dd15e70c657d906b2
Jerald Limqueco <jeraldlimqueco@yahoo.com>
2015-10-27 19:50:14 +0800

    menu activity
    lbycp24
    Jerald Limqueco

---
+++ BreastCancerAwarenessApp/app/src/main/java/com/example/lufamily/breastcancerawarenessapp/English.java
@@ -0,0 +1,111 @@
+package com.example.lufamily.breastcancerawarenessapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
+
+public class English extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_english);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                if(position == 0)
+                {
+                    Intent overview = new Intent(getApplicationContext(), OverviewAndFacts.class);
+                    startActivity(overview);
+                }
+
+                else if(position == 1)
+                {
+                    Intent symptoms = new Intent(getApplicationContext(), SymptomsAndTypes.class);
+                    startActivity(symptoms);
+                }
+
+                else if(position == 2)
+                {
+                    Intent diagnosis = new Intent(getApplicationContext(), DiagnosisAndTests.class);
+                    startActivity(diagnosis);
+                }
+
+                else if(position == 3)
+                {
+                    Intent treatment = new Intent(getApplicationContext(), TreatmentAndCare.class);
+                    startActivity(treatment);
+                }
+
+                else if(position == 4)
+                {
+                    Intent living = new Intent(getApplicationContext(), LivingAndManaging.class);
+                    startActivity(living);
+                }
+
+                else if(position == 5)
+                {
+                    Intent support = new Intent(getApplicationContext(), SupportAndResources.class);
+                    startActivity(support);
+                }
+            }
+        };
+        ListView listViewE = (ListView) findViewById(R.id.listViewEng);
+        listViewE.setOnItemClickListener(itemClickListener);
+    }
+
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
+
+    public void onClickShare(View v){
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
+            ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                    .setContentTitle("Breast Cancer App")
+                    .setContentDescription("This app raises awareness on Breast cancer")
+                    .setContentUrl(Uri.parse("https://github.com/DeLaSalleUniversity-Manila/breastcancerawarenessapp-ErvinLu"))
+                    .build();
+            shareDialog.show(linkContent);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_english, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
